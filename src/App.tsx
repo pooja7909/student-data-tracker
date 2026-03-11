e396f4f0a78c91d45cfb3f5d61cc519436d3acbc
pooja7909 <52460015+pooja7909@users.noreply.github.com>
2026-03-11 19:13:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://akzfkofiephdhqkqsboc.supabase.co';
-const supabaseKey = 'sb_publishable_nczB0oTnr8ZbB5-eTy3oHg_-QjvoMDk';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFremZrb2ZpZXBoZGhxYnFzYm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzcyODQsImV4cCI6MjA4ODgxMzI4NH0.NS1ymMoc_1alhtHypGj_qir9fnUfSjTJmjvug0lzbr4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 import { 
