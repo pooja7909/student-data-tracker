d0cde8f7ed7745745135c2ecb1b1ea653f3fb3e4
pooja7909 <52460015+pooja7909@users.noreply.github.com>
2026-03-11 16:40:25 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://akzfkofiephdhqkqsboc.supabase.co'
 const supabaseKey = 'sb_publishable_nczB0oTnr8ZbB5-eTy3oHg_-QjvoMDk'
 
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
